a4d864e514894ae67653b58aebfd8d4787251d2b
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-08 20:01:17 +0400

    feat: add cors and winston

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,23 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes');
 const handleErrors = require('./middlewares/handleErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DATABASE_PATH } = process.env;
+const { PORT = 4000, DATABASE_PATH } = process.env;
 mongoose.connect(DATABASE_PATH || 'mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 const app = express();
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(requestLogger);
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 app.use(handleErrors);
 
