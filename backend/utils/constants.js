b7ee0fe55485609bac79dbcf43dc0c6d058ea160
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-10 20:20:05 +0400

    fix: delete unused constances

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,6 +1,5 @@
 const OK_CODE = 200;
 const CREATED_CODE = 201;
-const BAD_REQUEST_CODE = 400;
 const UNAUTHORIZED_CODE = 401;
 const FORBIDDEN_CODE = 403;
 const NOT_FOUND_CODE = 404;
@@ -10,8 +9,6 @@ const SERVER_ERR_CODE = 500;
 const SERVER_ERR_MESSAGE = 'На сервере произошла ошибка.';
 const NOT_FOUND_USERID = 'Пользователь с указанным id не найден.';
 const NOT_FOUND_CARDID = 'Карточка с указанным id не найдена.';
-const NOT_FOUND_ROUTE = 'Запрашиваемый ресурс не найден.';
-const VALIDATION_ERR_MESSAGE = 'Ошибка валидации.';
 const UNAUTHORIZED_MESSAGE = 'Нет доступа.';
 const WRONG_CREDENTIALS_MESSAGE = 'Неправильные почта или пароль.';
 const FORBIDDEN_CARD_DELETE_MESSAGE = 'Нельзя удалять чужие карточки.';
@@ -21,7 +18,6 @@ const urlRegex = /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*\/?$/i;
 module.exports = {
   OK_CODE,
   CREATED_CODE,
-  BAD_REQUEST_CODE,
   NOT_FOUND_CODE,
   FORBIDDEN_CODE,
   CONFLICT_CODE,
@@ -31,12 +27,10 @@ module.exports = {
   SERVER_ERR_MESSAGE,
   NOT_FOUND_USERID,
   NOT_FOUND_CARDID,
-  NOT_FOUND_ROUTE,
   UNAUTHORIZED_MESSAGE,
   WRONG_CREDENTIALS_MESSAGE,
   FORBIDDEN_CARD_DELETE_MESSAGE,
   EXISTING_EMAIL_MESSAGE,
-  VALIDATION_ERR_MESSAGE,
   AUTH_SUCCESS_MESSAGE,
   urlRegex,
 };
