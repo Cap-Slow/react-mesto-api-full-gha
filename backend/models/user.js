ce2438a42030284efde056b3074b3774c2095c36
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-10 19:53:30 +0400

    fix: add min and max length for name and about fields in userSchema

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,17 +5,22 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     default: 'Жак-Ив Кусто',
+    minlength: 2,
+    maxlength: 30,
   },
   about: {
     type: String,
     default: 'Исследователь',
+    minlength: 2,
+    maxlength: 30,
   },
   avatar: {
     type: String,
     default:
       'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: (v) => /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*\/?$/i.test(v),
+      validator: (v) =>
+        /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*\/?$/i.test(v),
       message: 'Неверный формат ссылки',
     },
   },
