b1d0d45e7d3a51706be1b6f57ebdaa11cc5a7b01
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-10 19:51:11 +0400

    fix: delete excessive code

--- backend/middlewares/handleErrors.js
+++ backend/middlewares/handleErrors.js
@@ -1,22 +1,15 @@
-const { isCelebrateError } = require('celebrate');
 const {
   SERVER_ERR_CODE,
   SERVER_ERR_MESSAGE,
-  BAD_REQUEST_CODE,
   CONFLICT_CODE,
   CELEBRATE_CONFLICT_CODE,
   EXISTING_EMAIL_MESSAGE,
-  VALIDATION_ERR_MESSAGE,
 } = require('../utils/constants');
 
 module.exports = (err, req, res, next) => {
   const { statusCode = SERVER_ERR_CODE, message = SERVER_ERR_MESSAGE } = err;
   if (err.code === CELEBRATE_CONFLICT_CODE) {
     res.status(CONFLICT_CODE).send({ message: EXISTING_EMAIL_MESSAGE });
-    return;
-  }
-  if (isCelebrateError(err)) {
-    res.status(BAD_REQUEST_CODE).send({ message: VALIDATION_ERR_MESSAGE });
     return;
   }
   res.status(statusCode).send({
