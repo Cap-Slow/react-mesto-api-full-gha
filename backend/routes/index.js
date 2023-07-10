b6c72c33d2779cad7ef2f08e8aae12a4ea4d6fe8
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-10 20:09:29 +0400

    fix: pass a server error after requesting an unexisting route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,11 @@ const routes = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const auth = require('../middlewares/auth');
-const { NOT_FOUND_CODE, NOT_FOUND_ROUTE } = require('../utils/constants');
 
 routes.use('', userRoutes);
 routes.use('/cards', auth, cardRoutes);
-routes.use('*', (req, res) => {
-  res.status(NOT_FOUND_CODE).send({ message: NOT_FOUND_ROUTE });
+routes.use('*', (req, res, next) => {
+  next(new Error());
 });
 
 module.exports = routes;
