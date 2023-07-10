3f4e868c7410f6782a30112f1b492e179f06d5fa
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-10 21:21:41 +0400

    feat: import validators from separate file

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,5 +1,4 @@
 const cardRoutes = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const {
   getCards,
   createCard,
@@ -7,44 +6,27 @@ const {
   addCardLike,
   removeCardLike,
 } = require('../controllers/cards');
-const { urlRegex } = require('../utils/constants');
+const { cardBodydValidator, cardIdValidator } = require('../middlewares/requestValidators');
 
 cardRoutes.get('', getCards);
 cardRoutes.post(
   '',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().pattern(urlRegex),
-    }),
-  }),
+  cardBodydValidator,
   createCard,
 );
 cardRoutes.delete(
   '/:cardId',
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().hex().length(24),
-    }),
-  }),
+  cardIdValidator,
   deleteCard,
 );
 cardRoutes.put(
   '/:cardId/likes',
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().hex().length(24),
-    }),
-  }),
+  cardIdValidator,
   addCardLike,
 );
 cardRoutes.delete(
   '/:cardId/likes',
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().hex().length(24),
-    }),
-  }),
+  cardIdValidator,
   removeCardLike,
 );
 
