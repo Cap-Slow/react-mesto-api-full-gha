9a48e19cdcdb08f1cb8e2e62ef16e91a699c002b
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-11 19:06:09 +0400

    fix: create custom error while requesting nonexisting url

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ const routes = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const auth = require('../middlewares/auth');
+const NotFoundError = require('../utils/errors/notFoundError');
+const { NONEXISTENT_URL_MESSAGE } = require('../utils/constants');
 
 routes.use('', userRoutes);
 routes.use('/cards', auth, cardRoutes);
 routes.use('*', (req, res, next) => {
-  next(new Error());
+  next(new NotFoundError(NONEXISTENT_URL_MESSAGE));
 });
 
 module.exports = routes;
